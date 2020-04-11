36774902c46f9950390f0ec784d58092473441f4
Joseph Jinn <jj47@students.calvin.edu>
2020-04-11 13:06:51 -0400

    cleaning up run_generation_visualization_web_app.py

--- run_generation_visualization_web_app.py
+++ run_generation_visualization_web_app.py
@@ -5,9 +5,7 @@ Advisor: Professor Kenneth Arnold
 Coordinator: Professor Keith VanderLinden
 Author: Joseph Jinn
 
-run_generation_console_program.py defines and implements a bare-bones text modeling and prediction program using the GPT2
-    model and tokenizer.
-
+run_generation_visualization_web_app.py defines the backend for a Flask web app utilizing the GPT-2 model.
 #########################################################################################
 
 Notes:
@@ -27,7 +25,6 @@ Enter this in Terminal (for use with jupyter-themes):
 jt -t monokai -f fira -fs 13 -nf ptsans -nfs 11 -N -kl -cursw 5 -cursc r -cellw 95% -T
 
 #########################################################################################
-
 Important files to reference:
 
 modeling_gpt2.py
@@ -37,27 +34,6 @@ tokenization_gpy2.py
  - The tokenizer class for the GPT2 model.
 
 #########################################################################################
-
-Reference Material to understand the Theoretical Foundation of GPT2:
-https://en.wikipedia.org/wiki/Language_model
-http://karpathy.github.io/2015/05/21/rnn-effectiveness/
-
-It would also be helpful to have some concept about beam search… I’m not super-happy with what my Googling obtains but…
-https://en.wikipedia.org/wiki/Beam_search
-https://machinelearningmastery.com/beam-search-decoder-natural-language-processing/
-
-Also maybe helpful but don’t get distracted:
-the first 20 minutes or so of this (everything after that is details of training, skip it.)
-https://www.youtube.com/watch?v=Keqep_PKrY8
-https://medium.com/syncedreview/language-model-a-survey-of-the-state-of-the-art-technology-64d1a2e5a466
-https://s3-us-west-2.amazonaws.com/openai-assets/research-covers/language-unsupervised/language_understanding_paper.pdf
-https://d4mucfpksywv.cloudfront.net/better-language-models/language_models_are_unsupervised_multitask_learners.pdf
-http://colah.github.io/posts/2015-08-Understanding-LSTMs/
-
-https://stackoverflow.com/questions/34750268/extracting-the-top-k-value-indices-from-a-1-d-tensor
-https://stackoverflow.com/questions/53903373/convert-pytorch-tensor-to-python-list
-#########################################################################################
-
 More Notes:
 
 - CTRL + M + L (while in command mode): Adds code cell line numbers (very useful for debugging)
@@ -71,20 +47,18 @@ More Notes:
 
 - Shift + Escape (close API documentation panel)
 """
-
 #########################################################################################
 #########################################################################################
 
 # Import required packages and libraries.
 import torch  # PyTorch.
 import pandas as pd # Pandas.
+import random  # Random number generator.
 from tqdm import trange  # Instantly make your loops show a smart progress meter.
 from transformers import GPT2LMHeadModel, GPT2Tokenizer
 
 #########################################################################################
-
 # FIXME - need to associate chosen tokens for predicted sentence with token selected choice dictionary.
-
 # Load the GPT2-model.
 model_class = GPT2LMHeadModel  # Specifies the model to use.
 tokenizer_class = GPT2Tokenizer  # Specifies the tokenizer to use for the model.
@@ -96,48 +70,30 @@ model.eval()  # Specifies that the model is NOT in training mode.
 #########################################################################################
 
 
-# noinspection DuplicatedCode,PyUnresolvedReferences
 def extract_top_k_tokens(filtered_logits, k_value):
     """
-    This function utilizes the torch.topk() function to choose the "k" most likely words.
-
-    torch.topk performs a similar function to Softmax and argmax.
-    Uses the words' "scores" to choose the top "k" most likely predicted words (tokens).
-
-    - torch.topk
-     - Returns the :attr:`k` largest elements of the given :attr:`input` tensor along a given dimension.
-
-    Non-statistical and probabilistic method, so results are deterministic (always the same).
-
+    This function utilizes the torch.topk() function to choose the "k" word with the highest score values.
     Parameters:
         filtered_logits - entire vocabulary with assigned scores from GPT2 model.
         k_value - choose "k" top most likely words.
-
     Return:
-        my_topk - top "k" word tokens as Tensors.
+        my_topk - top "k" (most likely) word tokens as Tensors.
     """
-
-    # Return the top "k" most likely (highest score value) words in sorted order..
     my_topk = torch.topk(filtered_logits, k=k_value, dim=1, sorted=True)
-
-    # Returns the Tensor array of the top "k" word tokens
     return my_topk
 
 
 #########################################################################################
 
-# noinspection DuplicatedCode,PyUnresolvedReferences
 def prediction_generation(context_tokens, generated):
     """
-    This function makes text prediction using the GPT2 model and outputs the results.
-
+    This function makes text prediction using the GPT2 model and returns the results.
     Parameters:
        context_tokens - the encoded raw text string.
        generated - context_tokens wrapped as a PyTorch Tensor.
-       prediction_option - 'auto' or 'interactive' text prediction option.
+    Return:
+        [complete_string, token_options_all_lists] - list containing entire prediction and choice of tokens per word.
     """
-    import random  # Random number generator.
-
     temperature = 1  # Default value.
     iterations = 20  # Default value.
     k_value = 3  # Top "k" words to choose.
@@ -167,7 +123,7 @@ def prediction_generation(context_tokens, generated):
     iteration_counter = 0
 
     with torch.no_grad():  # This specifies not to use stochastic gradient descent!
-        for increament in trange(int(iterations)):
+        for increment in trange(int(iterations)):
 
             # Note: Feeding the results back into the model is the beginnings of a beam search algorithm.
             # Currently, randomly chooses one of the "generated" Tensors to feed back in.
@@ -310,7 +266,6 @@ def prediction_generation(context_tokens, generated):
 
 #########################################################################################
 
-# noinspection DuplicatedCode
 def main(user_input_string):
     """
     Main encodes the raw text string, wraps in PyTorch Tensor, and calls prediction_generation().
@@ -346,12 +301,7 @@ def main(user_input_string):
 
 
 #########################################################################################
-
 # Execute the program.
-# Select below and Run with "Alt + Shift + E" to avoid re-running entire fire and re-loading model every-time.
-# Note: you need to run the entire code-base at least once with Alt + Shift + E to have everything in memory.
-# Note: anytime you make changes to any function, need to re-run entire function for the changes to implement.
 if __name__ == '__main__':
     main("Hello")
-
 ############################################################################################
